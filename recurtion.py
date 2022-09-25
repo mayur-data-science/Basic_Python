828da01b00ff902ecb17f1639830bf0325c0e7d3
Mayur Adhude | मयूर आधुडे <mayur.adhude1998@gmail.com>
2022-09-26 01:25:14 +0530

    Recurtion in python

--- recurtion.py
+++ recurtion.py
@@ -1,14 +1,17 @@
-#Recurtion in python
+# Recurtion in python
 
 def factorial(num):
     if num == 1:
         return 1
     else:
                         # Calling function
-        return num   *  factorial(num-1)    # return 5 * factorial(4)  # 5 * 24 # return 120
-                                            # return 4 * factorial(3)  # 4 * 6  # return 24
-                                            # return 3 * factorial(2)  # 3 * 2  # return 6 
-                                            # return 2 * factorial(1)  # 2 * 1  # return 2
+        return num   *  factorial(num-1)    # return 5 * factorial(4)       # return 5 * 24 # return 120
+                                                        # Calling function
+                                            # return 4 * factorial(3)       # return 4 * 6  # return 24
+                                                        # Calling function
+                                            # return 3 * factorial(2)       # return 3 * 2  # return 6
+                                                        # Calling function
+                                            # return 2 * factorial(1)       # return 2 * 1  # return 2
                                             #               if True     # else bolc wont run bcz "if" conditon evaluates "True" so no function call again.
                                                                 # return 1
 num1 = 5
