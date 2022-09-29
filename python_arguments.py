c8a7e73028f2a131d04b5c7ef1dbfe96ac1a4d13
Mayur Adhude | मयूर आधुडे <mayur.adhude1998@gmail.com>
2022-09-29 14:39:42 +0530

    Python Arguments

---
+++ python_arguments.py
@@ -0,0 +1,12 @@
+# Python Arguments :
+
+
+# Python Arbitrary Arguments –
+#               In this, we use the asterisk (*) to denote this method before the parameter in the function. 
+#               This is also called as Python *args. 
+#               Python *args allows a function to accept any number of positional arguments 
+#               i.e. arguments which are non-keyword arguments, variable-length argument list.
+
+# Remember 'args' is just a notation. We can use any other argument name instead of it.
+
+# arbitrary arguments come in handy when we don’t know how many arguments are needed in the program at that moment.
