373444878f9f123971006c173bb25a589f08819e
Mayur Adhude | मयूर आधुडे <mayur.adhude1998@gmail.com>
2022-10-01 22:13:45 +0530

    Iterables: list, dict, tuple, and set

--- iterable.py
+++ iterable.py
@@ -1,1 +1,5 @@
 # Iterables: list, dict, tuple, and set
+    # In Python, an iterable object (or simply an iterable) is a collection of elements that you can loop (or iterate) through one element at a time.
+    
+    # An iterable is ordered if you can retrieve its elements in a predictable order
+    # An iterable is mutable if you can change which elements it contains
