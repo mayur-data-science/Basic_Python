f51fd1beb90ce62c6a9704f642992458457d0d84
Mayur Adhude | मयूर आधुडे <mayur.adhude1998@gmail.com>
2022-10-15 00:14:02 +0530

    Python Introduction

--- python_intoduction.py
+++ python_intoduction.py
@@ -1,3 +1,51 @@
-#--------------#
-# Introduction :
-#--------------#
+#---------------------#
+# Python Introduction :
+#---------------------#
+
+    # Python is a general purpose high level programming language.
+        # General Purpose --> Common purpose(That means we can use this programming language anywhere)
+        # High level programming language --> we need not to take care low level things( address of varibale ,pointers,Memory utilization , Datatype)
+        # such type of programming language are known as programmer friendly programing language.
+
+
+    # Python was developed by Guido Van Rossam in 1989 while working at National Research Institute at Netherlands.
+    # But officially Python was made available to public in 1991. The official Date of Birth for Python is : Feb 20th 1991.
+    # Python is recommended as first programming language for beginners.
+    # Python is implemented before java.(1996 jdk 1.0).
+
+    # The name Python was selected from the TV Show "The Complete Monty Python's Circus", which was broadcasted in BBC from 1969 to 1974.
+
+    # Guido developed Python language by taking almost all programming features from different languages
+            # 1.Functional Programming Features from C 
+            # 2.Object Oriented Programming Features from C++
+            # 3.Scripting Language Features from Perl and Shell Script 
+            # 4. Modular Programming Features from Modula-3
+
+    # Most of syntax in Python Derived from "C" and "ABC" languages.
+
+    # Where we can use Python:
+        # 1.Machine learning Algorithms--> scikit learn
+        # 2.Web Applications --> Django , Flask , tornado etc
+        # 3.Data science and Data Analysis --> Numpy , scipy, Pandas
+        # 4.Data visualization --> Matplotlib, Seaborn etc..
+        # 5.Iot --> Internet of Things --> AI --> Python
+        # 6.Game --> Pygame
+        # 7.Desktop Application--> Tkinter, flet
+        # 8.For developing database Applications
+        # 9.For Network Programming
+
+    # Worst case of using Python:
+        # 1.Mobile Application --> Java and Kotlin
+        # 2.For implementing compiler --> High level programming and it is slow as compared to java
+
+    # Note:
+        # Internally Google and Youtube use Python coding.
+        # NASA and Nework Stock Exchange Applications developed by Python.
+        # Top Software companies like Google, Microsoft, IBM, Yahoo, dropbox using Python.
+
+    
+    # Features of Python:
+    
+        # 1) Simple and easy to learn:
+            # Python is a simple programming language. When we read Python program,we can feel like reading english statements.
+            
