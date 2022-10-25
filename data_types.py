815c18b220e40ff48aaef87b625d3bb178a7ee9b
Mayur Adhude | मयूर आधुडे <mayur.adhude1998@gmail.com>
2022-10-25 14:07:44 +0530

    Data Types

--- data_types.py
+++ data_types.py
@@ -1,3 +1,37 @@
 #------------#
 # Data Types #
 #------------#
+        # Data Type represents the type of data present inside a variable.
+        # In Python we are not required to specify the type explicitly. 
+        # Based on value provided,the type will be assigned automatically.
+        # Hence Python is dynamically Typed Language.
+
+    # Python contains the following inbuilt data types:
+        # 1) Int
+        # 2) Float
+        # 3) Complex
+        # 4) Bool
+        # 5) Str
+        # 6) Bytes
+        # 7) Bytearray
+        # 8) Range
+        # 9) List
+        # 10) Tuple
+        # 11) Set
+        # 12) Frozenset
+        # 13) Dict
+        # 14) None
+
+    # Note : Python contains several inbuilt functions
+
+        # 1) type()  ---> to check the type of variable. 
+        # 2) id()    ---> to get address(memory location) of object in decimal form.
+        # 3) print() ---> to print the value.
+
+    # In Python everything is an Object.
+
+# 1)int Data Type:
+    # We can use int data type to represent whole numbers (integral values) 
+# Ex : 
+a = 10
+print(type(a)) # o/p: <class 'int'>
